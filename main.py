09ff56d869cda3791fcf7d14069952d32c58fb3c
YraKudasov <kudasov5@mail.ru>
2024-05-05 23:41:22 +0300

    10. change start to beginning

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from telebot import types
 bot = telebot.TeleBot('6883397009:AAE_zS_E1I8iTMuwu1AkHKXQMywJV-mZeQ0')
 
 
-@bot.message_handler(commands=['start'])
+@bot.message_handler(commands=['beginning']) # commit_10
 def newChat(message):
     bot.send_message(message.chat.id,
                      f'Здравствуйте, наш дорогой клиент.\nС каким <b><u>социальным вопросом</u></b> вам необходимо помочь?',parse_mode='HTML')
